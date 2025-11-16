2d1f610cc027d310305743b1f4d5c43e8542b5a3
osama dad <osamadad768@gmail.com>
2025-11-16 11:52:45 +0300

    feat: user service

---
+++ src/main/java/com/tuwaiq/ecommerce_system/Service/UserService.java
@@ -0,0 +1,50 @@
+package com.tuwaiq.ecommerce_system.Service;
+
+import com.tuwaiq.ecommerce_system.Model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+
+    ArrayList<User> users = new ArrayList<>();
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+    public Boolean updateUser(String id,User user){
+        for (int i=0;i<users.size()-1;i++) {
+            if (users.get(i).getId().equalsIgnoreCase(id)){
+                users.set(i,user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean deleteUser(String id){
+        for (User user:users){
+            if (user.getId().equalsIgnoreCase(id)){
+                users.remove(user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkIdValidation(String id) {
+        for (User user :users){
+            if (user.getId().equalsIgnoreCase(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
