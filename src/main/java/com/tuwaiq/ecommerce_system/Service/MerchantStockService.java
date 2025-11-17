2e246d3d148419436a72093ece44cba22e471488
osama dad <osamadad768@gmail.com>
2025-11-17 19:36:05 +0300

    feature: refactor buy product from user to merchant stock

--- src/main/java/com/tuwaiq/ecommerce_system/Service/MerchantStockService.java
+++ src/main/java/com/tuwaiq/ecommerce_system/Service/MerchantStockService.java
@@ -3,6 +3,7 @@ package com.tuwaiq.ecommerce_system.Service;
 import com.tuwaiq.ecommerce_system.Model.Merchant;
 import com.tuwaiq.ecommerce_system.Model.MerchantStock;
 import com.tuwaiq.ecommerce_system.Model.Product;
+import com.tuwaiq.ecommerce_system.Model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class MerchantStockService {
     ArrayList<MerchantStock> merchantStocks = new ArrayList<>();
     private final ProductService productService;
     private final MerchantService merchantService;
+    private final UserService userService;
 
     public String addMerchantStock(MerchantStock merchantStock) {
         String value = merchantProductValidation(merchantStock.getMerchantId(), merchantStock.getProductId());
@@ -74,6 +76,46 @@ public class MerchantStockService {
                 }
             } else {
                 errorType = "merchant id error";
+            }
+        }
+        return errorType;
+    }
+
+    public String buyProduct(String userId, String merchantId, String productId) {
+        String errorType="General error";
+        double productPrice = 0;
+        for (Product product : productService.getProducts()) {
+            if (product.getId().equalsIgnoreCase(productId)) {
+                productPrice = product.getPrice();
+            }else {
+                errorType="product id error";
+            }
+        }
+        for (User user : userService.users) {
+            if (user.getId().equalsIgnoreCase(userId)) {
+                for (MerchantStock merchantStock : merchantStocks) {
+                    if (merchantStock.getMerchantId().equalsIgnoreCase(merchantId)) {
+                        if (merchantStock.getProductId().equalsIgnoreCase(productId)) {
+                            if (merchantStock.getStock() > 0) {
+                                if (user.getBalance() >= productPrice) {
+                                    merchantStock.setStock(merchantStock.getStock()-1);
+                                    user.setBalance(user.getBalance()-productPrice);
+                                    return "ok";
+                                }else{
+                                    errorType="balance error";
+                                }
+                            }else {
+                                errorType="stock error";
+                            }
+                        }else {
+                            errorType="product error";
+                        }
+                    }else {
+                        errorType="merchant id error";
+                    }
+                }
+            }else {
+                errorType="user id error";
             }
         }
         return errorType;
