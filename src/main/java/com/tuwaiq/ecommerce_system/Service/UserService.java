33698013d2b9a732a46c3dfb4b56b8d2068ab9fa
osama dad <osamadad768@gmail.com>
2025-11-17 13:40:00 +0300

    feat: buy product

--- src/main/java/com/tuwaiq/ecommerce_system/Service/UserService.java
+++ src/main/java/com/tuwaiq/ecommerce_system/Service/UserService.java
@@ -1,36 +1,43 @@
 package com.tuwaiq.ecommerce_system.Service;
 
+import com.tuwaiq.ecommerce_system.Model.MerchantStock;
+import com.tuwaiq.ecommerce_system.Model.Product;
 import com.tuwaiq.ecommerce_system.Model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     ArrayList<User> users = new ArrayList<>();
+    private final MerchantStockService merchantStockService;
+    public final ProductService productService;
 
-    public void addUser(User user){
+    public void addUser(User user) {
         users.add(user);
     }
 
-    public ArrayList<User> getUsers(){
+    public ArrayList<User> getUsers() {
         return users;
     }
 
-    public Boolean updateUser(String id,User user){
-        for (int i=0;i<users.size();i++) {
-            if (users.get(i).getId().equalsIgnoreCase(id)){
-                users.set(i,user);
+    public Boolean updateUser(String id, User user) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId().equalsIgnoreCase(id)) {
+                users.set(i, user);
                 return true;
             }
         }
         return false;
     }
 
-    public Boolean deleteUser(String id){
-        for (User user:users){
-            if (user.getId().equalsIgnoreCase(id)){
+    public Boolean deleteUser(String id) {
+        for (User user : users) {
+            if (user.getId().equalsIgnoreCase(id)) {
                 users.remove(user);
                 return true;
             }
@@ -38,13 +45,43 @@ public class UserService {
         return false;
     }
 
-    private boolean checkIdValidation(String id) {
-        for (User user :users){
-            if (user.getId().equalsIgnoreCase(id)){
-                return true;
+    public int buyProduct(String userId, String merchantId, String productId) {
+        int errorType=1;
+        double productPrice = 0;
+        for (Product product : productService.getProducts()) {
+            if (product.getId().equalsIgnoreCase(productId)) {
+                productPrice = product.getPrice();
+            }else {
+                errorType=3;
             }
         }
-        return false;
+        for (User user : users) {
+            if (user.getId().equalsIgnoreCase(userId)) {
+                for (MerchantStock merchantStock : merchantStockService.getMerchantStocks()) {
+                    if (merchantStock.getMerchantId().equalsIgnoreCase(merchantId)) {
+                        if (merchantStock.getProductId().equalsIgnoreCase(productId)) {
+                            if (merchantStock.getStock() > 0) {
+                                if (user.getBalance() >= productPrice) {
+                                    merchantStock.setStock(merchantStock.getStock()-1);
+                                    user.setBalance(user.getBalance()-productPrice);
+                                    return 0;
+                                }else{
+                                    errorType=5;
+                                }
+                            }else {
+                                errorType=4;
+                            }
+                        }else {
+                            errorType=3;
+                        }
+                    }else {
+                        errorType=2;
+                    }
+                }
+                errorType=1;
+            }
+        }
+        return errorType;
     }
 
 }
